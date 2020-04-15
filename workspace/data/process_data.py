0a355c8710c003c27cce7a17eaae5fe1651cd562
fsteppich <fsteppich@gmail.com>
2020-04-15 21:17:50 +0200

    Finish Exploration
    Finish the try and error process and refactor into usable code.
    
    Resolves: -
    See also: -

--- workspace/data/process_data.py
+++ workspace/data/process_data.py
@@ -36,7 +36,7 @@ def main():
               'well as the filepath of the database to save the cleaned data '\
               'to as the third argument. \n\nExample: python process_data.py '\
               'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
+              'DisasterResponse.sqlite')
 
 
 if __name__ == '__main__':
